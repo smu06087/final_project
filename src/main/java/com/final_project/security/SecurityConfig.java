b755302dd496fcc22aea546f5468f31c974b579d
박재운 <smu06089@gmail.com>
2026-04-09 11:14:12 +0900

    MemberMapper수정

--- src/main/java/com/final_project/security/SecurityConfig.java
+++ src/main/java/com/final_project/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
 
         http.authorizeHttpRequests(config -> config
-            .requestMatchers("/home", "/login", "/join", "/api/test").permitAll()
+            .requestMatchers("/*").permitAll()
             .requestMatchers("/forgot-password", "/reset-password").permitAll()
             .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
             .anyRequest().authenticated()
